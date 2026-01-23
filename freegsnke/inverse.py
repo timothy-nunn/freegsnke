1fe04e18b523d39dc8ec141ac35e1e4f466cc1e9
Timothy Nunn <timothy.nunn@ukaea.uk>
2026-01-23 14:41:30 +0000

    Add slack to coil limits

--- freegsnke/inverse.py
+++ freegsnke/inverse.py
@@ -412,49 +412,58 @@ class Inverse_optimizer:
                 )
             reg_matrix = np.diag(l2_reg)
 
-        delta_current = None
         if self.coil_current_limits is not None:
-            delta = cvxpy.Variable(self.n_control_coils)
-
-            coil_upper_limits, coil_lower_limits = self.coil_current_limits
-            coil_limits = []
-            for coil_index, ul in enumerate(coil_upper_limits):
-                if ul is not None:
-                    print(
-                        f"{full_currents_vec[self.control_mask][coil_index]} + {delta} <= {ul}"
-                    )
-                    coil_limits.append(
-                        full_currents_vec[self.control_mask][coil_index]
-                        + delta[coil_index]
-                        <= ul
-                    )
-
-            for coil_index, ll in enumerate(coil_lower_limits):
-                if ll is not None:
-                    print(
-                        f"{full_currents_vec[self.control_mask][coil_index]} + {delta} >= {ll}"
-                    )
-                    coil_limits.append(
-                        full_currents_vec[self.control_mask][coil_index]
-                        + delta[coil_index]
-                        >= ll
-                    )
-
-            problem = cvxpy.Problem(
-                cvxpy.Minimize(
-                    cvxpy.sum_squares(self.A @ delta - self.b)
-                    + cvxpy.sum_squares(reg_matrix @ delta)
-                ),
-                coil_limits or None,
+            delta_current, loss = self.optimize_currents_quadratic(
+                full_currents_vec, reg_matrix
             )
-            problem.solve()
-            delta_current = delta.value
+        else:
+            delta_current = np.linalg.solve(
+                self.A.T @ self.A + reg_matrix, self.A.T @ self.b
+            )
+            loss = np.linalg.norm(self.loss)
 
-        if delta_current is None:
-            mat = np.linalg.inv(np.matmul(self.A.T, self.A) + reg_matrix)
-            delta_current = np.dot(mat, np.dot(self.A.T, self.b))
+        return delta_current, loss
 
-        return delta_current, np.linalg.norm(self.loss)
+    def optimize_currents_quadratic(self, full_currents_vec, reg_matrix, *, mu=1e5):
+        delta = cvxpy.Variable(self.n_control_coils)
+        coil_upper_slack = cvxpy.Variable(self.n_control_coils, nonneg=True)
+        coil_lower_slack = cvxpy.Variable(self.n_control_coils, nonneg=True)
+
+        coil_slack_scale = mu * np.diag(self.A.T @ self.A).max()
+
+        coil_upper_limits, coil_lower_limits = self.coil_current_limits
+        coil_limits = []
+        for coil_index, ul in enumerate(coil_upper_limits):
+            if ul is not None:
+                coil_limits.append(
+                    full_currents_vec[self.control_mask][coil_index] + delta[coil_index]
+                    <= ul + coil_upper_slack[coil_index]
+                )
+
+        for coil_index, ll in enumerate(coil_lower_limits):
+            if ll is not None:
+                coil_limits.append(
+                    full_currents_vec[self.control_mask][coil_index] + delta[coil_index]
+                    >= ll - coil_lower_slack[coil_index]
+                )
+
+        problem = cvxpy.Problem(
+            cvxpy.Minimize(
+                cvxpy.sum_squares(self.A @ delta - self.b)
+                + cvxpy.quad_form(delta, reg_matrix)
+                + cvxpy.sum_squares(coil_slack_scale * coil_upper_slack)
+                + cvxpy.sum_squares(coil_slack_scale * coil_lower_slack)
+            ),
+            coil_limits or None,
+        )
+        problem.solve(solver=cvxpy.CLARABEL)
+
+        return (
+            delta.value,
+            np.linalg.norm(self.loss)
+            + coil_upper_slack.value.sum()
+            + coil_lower_slack.value.sum(),
+        )
 
     def coil_current_limit_constraint(
         self,
